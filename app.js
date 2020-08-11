a1b81c248651754b8ec30ba8e88f2b2054e556bf
kellyco2019 <48420443+kellyco2019@users.noreply.github.com>
2020-08-11 10:47:12 -0500

    Add files via upload
    " mensajes imprimiendo"

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 //imprimir los mensajes desde un array 
 //cuando se presione enviar y la caja de texto tenga algo agregar el mensaje al array y al DOM
 
-const $chat = document.querySelector('.chat');
+const $input = document.querySelector(".input");
+const $enviar = document.querySelector(".enviar");
+const $chat = document.querySelector(".chat");
+
+$enviar.addEventListener("click",() => {
+     event.preventDefault()   
+     const value = $input.value;
+
+     if (value !== "") {
+        const now = new Date();
+
+        const mensajes = {
+        time: `${now.getHours()}:${now.getMinutes()}`,
+        content: value,
+        author: {
+            name: "kellyta",
+            image: "https://lorempixel.com/output/people-q-g-50-50-1.jpg",
+        },
+    };
+        printMensajes(mensajes);
+        mensaje.push(mensajes);
+        $input.value = "";
+    }   
+});
+
+
 const  mensajes = [
     {
     time: "11:02",
@@ -29,17 +54,25 @@ const  mensajes = [
     },
 ];
 
-function printMessage(mensajes) { 
-    const tamplate = <li>
-     <img class= "imagperfil" src="${author.image}" alt="imagen-perfil" />
-     <strong class="nombre">  ${author.name} </strong>   
-     <span class="time-right"> ${time}</span>
-     <span class="texto"> ${content}</span>
-     </li>;
-    
-     console.log(template);
+function printMensajes(mensajes) { 
+    const {author, time, content} = mensajes;
+   
+    const template = `<li class="cajamensajes">
+    <div> 
+        <img class="imagperfil" src="${author.image}" alt="imagen-perfil">
+        </div>
+        <div class="contenido">
+        <h4 class="nombre">${author.name}</h4>   
+        <p class="time-right">${time}</p>
+        <p class="texto"> ${content}</p>
+    </div>
+    </li>`;
  
+    $chat.innerHTML += template;
+
 }  
-    for (let i= 0; i < mensajes.length ; i++ ) {
-    printMessage(mensajes[i]);
-}
+mensajes.forEach((mensajes) => {
+    printMensajes(mensajes);
+//for (let i= 0; i < mensajes.length ; i++ ) {
+    //printMessage(mensajes[i]);
+});   
